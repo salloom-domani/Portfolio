6f734a40f89930e4feee02222493489b9dd4fa85
Salem Domani <salem.domani@gmail.com>
2022-03-15 16:16:43 +0200

    Better dark mode transition.

--- js/script.js
+++ js/script.js
@@ -1,6 +1,7 @@
 const body = document.querySelector('html');
 const toggleDarkMode = document.getElementById('toggle-dark-mode');
-const cover = document.querySelector('.cover img');
+const darkCover = document.getElementById('dark-cover');
+const lightCover = document.getElementById('light-cover');
 
 toggleDarkMode.dark = false;
 toggleDarkMode.addEventListener('click',() => {
@@ -8,12 +9,14 @@ toggleDarkMode.addEventListener('click',() => {
     if (!toggleDarkMode.dark) {
         body.style.setProperty('--background-color','#222');
         body.style.setProperty('--text-color','#fff');
-        cover.src = './images/code-light.png';
+        darkCover.style.setProperty('opacity',0);
+        lightCover.style.setProperty('opacity',1);
     }
     else {
         body.style.setProperty('--background-color','#fff');
         body.style.setProperty('--text-color','#37352f');
-        cover.src = './images/code-dark.png';
+        darkCover.style.setProperty('opacity',1);
+        lightCover.style.setProperty('opacity',0);
     }
     toggleDarkMode.dark = !toggleDarkMode.dark;
 })
