931251a9c65a7d787fd4dc39b0fe330454209e62
Salem Domani <salem.domani@gmail.com>
2022-03-16 12:07:06 +0200

    Add sun to light mode.

--- js/script.js
+++ js/script.js
@@ -1,22 +1,40 @@
-const body = document.querySelector('html');
-const toggleDarkMode = document.getElementById('toggle-dark-mode');
-const darkCover = document.getElementById('dark-cover');
-const lightCover = document.getElementById('light-cover');
+const body = document.querySelector("html");
+const toggleDarkMode = document.getElementById("toggle-dark-mode");
+const darkCover = document.getElementById("dark-cover");
+const lightCover = document.getElementById("light-cover");
+const moon = document.querySelector(".fa-moon");
+// const sun = document.querySelector(".fa-sun");
 
 toggleDarkMode.dark = false;
-toggleDarkMode.addEventListener('click',() => {
+// sun.style.setProperty("opacity", 0);
+
+toggleDarkMode.addEventListener("click", () => {
+  if (!toggleDarkMode.dark) {
+    body.style.setProperty("--background-color", "#222");
+    body.style.setProperty("--text-color", "#fff");
+
+    darkCover.style.setProperty("opacity", 0);
+    lightCover.style.setProperty("opacity", 1);
+
+    // sun.style.setProperty("opacity", 1);
+    // moon.style.setProperty("opacity", 0);
     
-    if (!toggleDarkMode.dark) {
-        body.style.setProperty('--background-color','#222');
-        body.style.setProperty('--text-color','#fff');
-        darkCover.style.setProperty('opacity',0);
-        lightCover.style.setProperty('opacity',1);
-    }
-    else {
-        body.style.setProperty('--background-color','#fff');
-        body.style.setProperty('--text-color','#37352f');
-        darkCover.style.setProperty('opacity',1);
-        lightCover.style.setProperty('opacity',0);
-    }
-    toggleDarkMode.dark = !toggleDarkMode.dark;
-})
+
+  } else {
+    body.style.setProperty("--background-color", "#fff");
+    body.style.setProperty("--text-color", "#37352f");
+
+    darkCover.style.setProperty("opacity", 1);
+    lightCover.style.setProperty("opacity", 0);
+
+    // sun.style.setProperty("opacity", 0);
+    // moon.style.setProperty("opacity", 1);
+  }
+  
+  setTimeout(() => {
+      moon.classList.toggle('fa-moon');
+      moon.classList.toggle('fa-sun');
+  }, 100)
+
+  toggleDarkMode.dark = !toggleDarkMode.dark;
+});
