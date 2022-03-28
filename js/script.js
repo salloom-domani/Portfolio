49844be6a3f26565c3d86015c7ddad9111403de4
Salem Domani <salem.domani@gmail.com>
2022-03-28 13:10:22 +0300

    Improve script readability.

--- js/script.js
+++ js/script.js
@@ -1,47 +1,54 @@
-const body = document.querySelector("html");
-const toggleDarkMode = document.getElementById("toggle-dark-mode");
-const toggleMoon = document.querySelector(".toggle-moon");
-const darkCover = document.getElementById("dark-cover");
-const lightCover = document.getElementById("light-cover");
-const moon = document.querySelector(".fa-moon");
-// const sun = document.querySelector(".fa-sun");
+function toggleDark() {
+  html.style.setProperty("--background-color", "#222");
+  html.style.setProperty("--text-color", "#fff");
 
-toggleDarkMode.dark = false;
-// sun.style.setProperty("opacity", 0);
+  darkCover.style.setProperty("opacity", 0);
+  lightCover.style.setProperty("opacity", 1);
+}
 
-toggleDarkMode.addEventListener("click", () => {
-  if (!toggleDarkMode.dark) {
-    body.style.setProperty("--background-color", "#222");
-    body.style.setProperty("--text-color", "#fff");
+function toggleLight() {
+  html.style.setProperty("--background-color", "#fff");
+  html.style.setProperty("--text-color", "#37352f");
 
-    darkCover.style.setProperty("opacity", 0);
-    lightCover.style.setProperty("opacity", 1);
+  darkCover.style.setProperty("opacity", 1);
+  lightCover.style.setProperty("opacity", 0);
+}
 
-    // sun.style.setProperty("opacity", 1);
-    // moon.style.setProperty("opacity", 0);
-    
-
-  } else {
-    body.style.setProperty("--background-color", "#fff");
-    body.style.setProperty("--text-color", "#37352f");
-
-    darkCover.style.setProperty("opacity", 1);
-    lightCover.style.setProperty("opacity", 0);
-
-    // sun.style.setProperty("opacity", 0);
-    // moon.style.setProperty("opacity", 1);
-  }
-  
+function toggleIcon() {
   setTimeout(() => {
-      moon.classList.toggle('fa-moon');
-      moon.classList.toggle('fa-sun');
-  }, 200);
+    moon.classList.toggle('fa-moon');
+    moon.classList.toggle('fa-sun');
+}, 200);
+}
 
+function setActive() {
   toggleMoon.classList.add('active');
 
   setTimeout(() =>{
     toggleMoon.classList.remove('active');
   }, 400);
+}
 
-  toggleDarkMode.dark = !toggleDarkMode.dark;
+const html = document.querySelector("html");
+const toggleDarkBtn = document.getElementById("toggle-dark-mode");
+const toggleMoon = document.querySelector(".toggle-moon");
+const darkCover = document.getElementById("dark-cover");
+const lightCover = document.getElementById("light-cover");
+const moon = document.querySelector(".fa-moon");
+
+toggleDarkBtn.dark = false;
+
+
+toggleDarkBtn.addEventListener("click", () => {
+  if (!toggleDarkBtn.dark) {
+    toggleDark();
+  } else {
+    toggleLight();
+  }
+  
+  toggleIcon();
+
+  setActive();
+
+  toggleDarkBtn.dark = !toggleDarkBtn.dark;
 });
